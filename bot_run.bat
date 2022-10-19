46d46eb45fa53fd4bbb3151e33f380290591c74b
albert <strannoo1@gmail.com>
2022-10-19 13:12:08 +0500

    update 3.1: обновил бат файл и подключение к гизмо

--- bot_run.bat
+++ bot_run.bat
@@ -3,7 +3,8 @@
 call %~dp0venv\Scripts\activate
 
 set TOKEN=5721543457:AAHWQannTuyFu_fCEkRep6cnjvZuN-cUcto
-
+set LOGIN=login
+set PASSWORD=password
 python bot_telegram.py
 
 pause
--- gizmo/connect_gizmo.py
+++ gizmo/connect_gizmo.py
@@ -1,7 +1,7 @@
 import requests
-
-login = "albert"
-password = "admin"
+import os
+login = os.getenv('LOGIN')
+password = os.getenv('PASSWORD')
 print(requests.get("http://"+login+":"+password+"@127.0.0.1:80/api/reservations"))
 response = requests.get("http://"+login+":"+password+"@127.0.0.1:80/api/reservations")
 print(response.text)
