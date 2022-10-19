ae8d64fcb5ea0b196f3c23607d83f6b8696c3d02
albert <strannoo1@gmail.com>
2022-10-19 15:08:05 +0500

    update 3.4.1: обновил connect_gizmo.py

--- gizmo/connect_gizmo.py
+++ gizmo/connect_gizmo.py
@@ -1,7 +1,13 @@
 import requests
 import os
+
 login = os.getenv('LOGIN')
 password = os.getenv('PASSWORD')
-print(requests.get("http://"+login+":"+password+"@127.0.0.1:80/api/reservations"))
-response = requests.get("http://"+login+":"+password+"@127.0.0.1:80/api/reservations")
-print(response.text)
+
+
+async def connect_to_ufa():
+    print(requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api"))
+
+
+async def connect_to_dema():
+    print(requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api"))
