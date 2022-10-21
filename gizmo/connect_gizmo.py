538bda348147c32e561444011b3c7ede52870006
albert <strannoo1@gmail.com>
2022-10-21 13:19:56 +0500

    update 3.9: добавил начало парсинга брони, надо исправить ошибки

--- gizmo/connect_gizmo.py
+++ gizmo/connect_gizmo.py
@@ -2,6 +2,7 @@ import random
 from gizmo import parsing_text
 import requests
 import os
+import re
 
 login = os.getenv('LOGIN')
 password = os.getenv('PASSWORD')
@@ -12,9 +13,34 @@ async def connect_to_ufa():
     if response.status_code == 200:
 
         print("Подключение к гизмо ТЦБ успешно\n")
-        response_activeinfo = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/usersessions/activeinfo")
-        response_active = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/usersessions/active")
-        status_pc_array = await parsing_text.parsing_user_sessions_active_info(response_activeinfo.text, response_active.text)
+        response = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/hostcomputers")
+        # test
+        """
+        new_text = response.text[response.text.find('[') + 2:len(response.text)]
+        new_text = new_text.replace('[', '')
+        new_text = new_text.replace('{', '')
+        new_text = new_text.replace('}', '')
+        new_text = new_text.replace(']', '')
+        new_text = new_text.replace('"','')
+        new_array = new_text.split(',')
+        i = 0
+        for g in range(len(new_array)):
+            new_array[g] = new_array[g].split(':')
+        while i < len(response.text):
+            new_i = response.text.find('name', i, len(response.text))
+            if new_i == -1:
+                break
+            new_i = response.text.find(':', new_i, len(response.text)) + 2
+            split_text = response.text.find(',', new_i, len(response.text)) - 1
+            current_pc = response.text[new_i:split_text]
+            i = new_i
+        for g in new_array:
+            print(g)
+        # test
+        """
+        # response_activeinfo = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/usersessions/activeinfo")
+        # response_active = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/usersessions/active")
+        # status_pc_array = await parsing_text.parsing_user_sessions_active_info(response_activeinfo.text, response_active.text)
     else:
         print("Ошибка подключения к гизмо ТЦБ: ", response.status_code)
 
@@ -29,9 +55,11 @@ async def connect_to_dema():
 
 async def get_all_status_pc(club_name: str):
     if club_name == "ufa":
-        response_activeinfo = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/usersessions/activeinfo")
+        response_activeinfo = requests.get(
+            "http://" + login + ":" + password + "@127.0.0.1:80/api/usersessions/activeinfo")
         response_active = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/usersessions/active")
-        status_pc_array = await parsing_text.parsing_user_sessions_active_info(response_activeinfo.text, response_active.text)
+        status_pc_array = await parsing_text.parsing_user_sessions_active_info(response_activeinfo.text,
+                                                                               response_active.text)
         return status_pc_array
     else:
         print()  # dema
--- gizmo/parsing_text.py
+++ gizmo/parsing_text.py
@@ -1,7 +1,10 @@
 import datetime
+import json
+
+"""          parsing_user_sessions_active_info      ПОКА НЕ РАБОТАЕТ   """
 
 
-async def parsing_user_sessions_active_info(text_for_number_pc: str, text_for_time : str):
+async def parsing_user_sessions_active_info(text_for_number_pc: str, text_for_time: str):
     counts_pc = 0
     with open("global_info/info_ufa.txt", "r") as f:
         counts_pc = int(f.readline())
@@ -21,7 +24,7 @@ async def parsing_user_sessions_active_info(text_for_number_pc: str, text_for_ti
         split_text = text_for_number_pc.find(',', new_i, len(text_for_number_pc))
         current_pc = int(text_for_number_pc[new_i:split_text])
         if current_pc < 21:
-            states_pc[current_pc-1][0] = current_pc
+            states_pc[current_pc - 1][0] = current_pc
         elif 20 < current_pc < 26:
             states_pc[current_pc - 6][0] = current_pc
         elif current_pc < 51:
@@ -41,18 +44,11 @@ async def parsing_user_sessions_active_info(text_for_number_pc: str, text_for_ti
         split_text = text_for_time.find(',', new_i, len(text_for_number_pc))
         new_text = text_for_time[new_i:split_text]
         end_time_year = int(new_text[0:new_text.find("-")])
-        end_time_month = int(new_text[new_text.find("-") + 1: new_text.find("-", new_text.find("-")+1)])
-        end_time_day = int(new_text[new_text.find("-", new_text.find("-")+1)+1:new_text.find("T")])
-        end_time_hours = int(new_text[new_text.find("T")+1:new_text.find(":")])
-        end_time_minutes = int(new_text[new_text.find(":")+1:new_text.find(":", new_text.find(":")+1)])
+        end_time_month = int(new_text[new_text.find("-") + 1: new_text.find("-", new_text.find("-") + 1)])
+        end_time_day = int(new_text[new_text.find("-", new_text.find("-") + 1) + 1:new_text.find("T")])
+        end_time_hours = int(new_text[new_text.find("T") + 1:new_text.find(":")])
+        end_time_minutes = int(new_text[new_text.find(":") + 1:new_text.find(":", new_text.find(":") + 1)])
         # end_time_seconds = new_text[new_text.find(":", new_text.find(":")+1):new_text.find(".")]
-
-
-
-
-
-
-
 
         current_span = float(new_text)
         states_pc[j][1] = current_span
@@ -61,42 +57,44 @@ async def parsing_user_sessions_active_info(text_for_number_pc: str, text_for_ti
     for i in range(25):  # пока не работает ⚫
         if i < 10:
             if states_pc[i][1] == -1:
-                status_pc_str += "⚫   Premium " + str(i+1) + "\n\n"
+                status_pc_str += "⚫   Premium " + str(i + 1) + "\n\n"
             elif states_pc[i][1] > 0:
                 time_minutes = int(states_pc[i][1] / 60)
                 time_hours = int(time_minutes / 24)
-                status_pc_str += "🔴   Premium " + str(i+1) + "  время:  " + str(time_hours) + " ч " + str(time_minutes) + " м" + "\n\n"
+                status_pc_str += "🔴   Premium " + str(i + 1) + "  время:  " + str(time_hours) + " ч " + str(
+                    time_minutes) + " м" + "\n\n"
             elif states_pc[i][1] == 0:
-                status_pc_str += "🟢   Premium " + str(i+1) + "\n\n"
+                status_pc_str += "🟢   Premium " + str(i + 1) + "\n\n"
         elif i < 15:
             if states_pc[i][1] == -1:
-                status_pc_str += "⚫   BootCamp " + str(i+1) + "\n\n"
+                status_pc_str += "⚫   BootCamp " + str(i + 1) + "\n\n"
             elif states_pc[i][1] > 0:
                 time_minutes = int(states_pc[i][1] / 60)
                 time_hours = int(time_minutes / 24)
-                status_pc_str += "🔴   BootCamp " + str(i+1) + "  время:  " + str(time_hours) + " ч " + str(time_minutes) + " м" + "\n\n"
+                status_pc_str += "🔴   BootCamp " + str(i + 1) + "  время:  " + str(time_hours) + " ч " + str(
+                    time_minutes) + " м" + "\n\n"
             elif states_pc[i][1] == 0:
-                status_pc_str += "🟢   BootCamp " + str(i+1) + "\n\n"
+                status_pc_str += "🟢   BootCamp " + str(i + 1) + "\n\n"
         elif i < 20:
             if states_pc[i][1] == -1:
-                status_pc_str += "⚫   BootCamp " + str(i+6) + "\n\n"
+                status_pc_str += "⚫   BootCamp " + str(i + 6) + "\n\n"
             elif states_pc[i][1] > 0:
                 time_minutes = int(states_pc[i][1] / 60)
                 time_hours = int(time_minutes / 24)
-                status_pc_str += "🔴   BootCamp " + str(i+6) + "  время:  " + str(
+                status_pc_str += "🔴   BootCamp " + str(i + 6) + "  время:  " + str(
                     time_hours) + " ч " + str(time_minutes) + " м" + "\n\n"
             elif states_pc[i][1] == 0:
-                status_pc_str += "🟢   BootCamp " + str(i+6) + "\n\n"
+                status_pc_str += "🟢   BootCamp " + str(i + 6) + "\n\n"
         elif i < 23:
             if states_pc[i][1] == -1:
-                status_pc_str += "⚫   Luxe " + str(i+26) + "\n\n"
+                status_pc_str += "⚫   Luxe " + str(i + 26) + "\n\n"
             elif states_pc[i][1] > 0:
                 time_minutes = int(states_pc[i][1] / 60)
                 time_hours = int(time_minutes / 24)
-                status_pc_str += "🔴   Luxe " + str(i+26) + "  время:  " + str(
+                status_pc_str += "🔴   Luxe " + str(i + 26) + "  время:  " + str(
                     time_hours) + " ч " + str(time_minutes) + " м" + "\n\n"
             elif states_pc[i][1] == 0:
-                status_pc_str += "🟢   Luxe " + str(i+26) + "\n\n"
+                status_pc_str += "🟢   Luxe " + str(i + 26) + "\n\n"
         elif i < 24:
             if states_pc[i][1] == -1:
                 status_pc_str += "⚫   Luxe 50\n\n"
@@ -119,3 +117,23 @@ async def parsing_user_sessions_active_info(text_for_number_pc: str, text_for_ti
     status_pc_str += "\nТекущий статус компьютеров и ps:\n⚫ - недоступен\n🔴 - занят\n🟢 - свободен"
 
     return status_pc_str
+
+
+async def parsing_reservations_for_pc(date: str, contact_phone: str, note: str, duration: int, hosts: list):
+    with open("generate_text/reservation_create.json") as f:
+        reservation_create_blank = json.load(f)
+    pin = "dsdado"
+    reservation_create_blank['date'] = "tt"
+    reservation_create_blank['duration'] = duration
+    reservation_create_blank['contactPhone'] = contact_phone
+    reservation_create_blank['contactEmail'] = ""
+    reservation_create_blank['note'] = note
+    reservation_create_blank['pin'] = pin
+    reservation_create_blank['status'] = 0
+    reservation_create_blank['hosts'] = []
+    for i in range(len(hosts)):
+        reservation_create_blank['hosts'].append({'hostId': hosts[i]})
+    print()
+
+
+
