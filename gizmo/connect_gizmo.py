cd667a9207163ad34087ece325eb7a10783b7822
albert <strannoo1@gmail.com>
2022-10-21 13:27:38 +0500

    update 3.9.3 надо исправить ошибки, добавил ip_ufa и ip_dema

--- gizmo/connect_gizmo.py
+++ gizmo/connect_gizmo.py
@@ -6,16 +6,16 @@ import re
 
 login = os.getenv('LOGIN')
 password = os.getenv('PASSWORD')
-ip = os.getenv("IP")
+ip_ufa = os.getenv("IPUFA")
 port = os.getenv("PORT")
-
+ip_dema = os.getenv("IPDEMA")
 
 async def connect_to_ufa():
-    response = requests.get("http://" + login + ":" + password + "@" + ip + ":" + port + "/api/usersessions/activeinfo")
+    response = requests.get("http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/usersessions/activeinfo")
     if response.status_code == 200:
 
         print("Подключение к гизмо ТЦБ успешно\n")
-        response = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/hostcomputers")
+        response = requests.get("http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "@127.0.0.1:80/api/hostcomputers")
         # test
         """
         new_text = response.text[response.text.find('[') + 2:len(response.text)]
@@ -48,7 +48,7 @@ async def connect_to_ufa():
 
 
 async def connect_to_dema():
-    response = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/stats/session")
+    response = requests.get("http://" + login + ":" + password + "@" + ip_dema + ":" + port + "/api/usersessions/activeinfo")
     if response.status_code == 200:
         print("Подключение к гизмо Дема успешно")
     else:
@@ -57,9 +57,8 @@ async def connect_to_dema():
 
 async def get_all_status_pc(club_name: str):
     if club_name == "ufa":
-        response_activeinfo = requests.get(
-            "http://" + login + ":" + password + "@127.0.0.1:80/api/usersessions/activeinfo")
-        response_active = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/usersessions/active")
+        response_activeinfo = requests.get("http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/usersessions/activeinfo")
+        response_active = requests.get("http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/usersessions/active")
         status_pc_array = await parsing_text.parsing_user_sessions_active_info(response_activeinfo.text,
                                                                                response_active.text)
         return status_pc_array
