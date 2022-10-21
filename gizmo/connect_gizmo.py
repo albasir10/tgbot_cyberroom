0f4f50aea3e43a8b5ed5f7492c3e0333f20b25c7
albert <strannoo1@gmail.com>
2022-10-21 20:13:32 +0500

    update 3.5.1: чутка сделал парсинг, не работает

--- gizmo/connect_gizmo.py
+++ gizmo/connect_gizmo.py
@@ -10,45 +10,24 @@ ip_ufa = os.getenv("IPUFA")
 port = os.getenv("PORT")
 ip_dema = os.getenv("IPDEMA")
 
-async def connect_to_ufa():
-    response = requests.get("http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/usersessions/activeinfo")
-    if response.status_code == 200:
 
+async def connect_to_ufa():
+    response = requests.get(
+        "http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/usersessions/activeinfo")
+    if response.status_code == 200:
         print("Подключение к гизмо ТЦБ успешно\n")
-        response = requests.get("http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "@127.0.0.1:80/api/hostcomputers")
-        # test
-        """
-        new_text = response.text[response.text.find('[') + 2:len(response.text)]
-        new_text = new_text.replace('[', '')
-        new_text = new_text.replace('{', '')
-        new_text = new_text.replace('}', '')
-        new_text = new_text.replace(']', '')
-        new_text = new_text.replace('"','')
-        new_array = new_text.split(',')
-        i = 0
-        for g in range(len(new_array)):
-            new_array[g] = new_array[g].split(':')
-        while i < len(response.text):
-            new_i = response.text.find('name', i, len(response.text))
-            if new_i == -1:
-                break
-            new_i = response.text.find(':', new_i, len(response.text)) + 2
-            split_text = response.text.find(',', new_i, len(response.text)) - 1
-            current_pc = response.text[new_i:split_text]
-            i = new_i
-        for g in new_array:
-            print(g)
-        # test
-        """
-        # response_activeinfo = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/usersessions/activeinfo")
-        # response_active = requests.get("http://" + login + ":" + password + "@127.0.0.1:80/api/usersessions/active")
+        await parsing_text.parsing_user_sessions_active_info(login, password, ip_ufa, port)
+        # response_activeinfo = requests.get("http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/usersessions/activeinfo")
+        # response_active = requests.get("http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/usersessions/active")
         # status_pc_array = await parsing_text.parsing_user_sessions_active_info(response_activeinfo.text, response_active.text)
+        # print(status_pc_array)
     else:
         print("Ошибка подключения к гизмо ТЦБ: ", response.status_code)
 
 
 async def connect_to_dema():
-    response = requests.get("http://" + login + ":" + password + "@" + ip_dema + ":" + port + "/api/usersessions/activeinfo")
+    response = requests.get(
+        "http://" + login + ":" + password + "@" + ip_dema + ":" + port + "/api/usersessions/activeinfo")
     if response.status_code == 200:
         print("Подключение к гизмо Дема успешно")
     else:
@@ -57,8 +36,10 @@ async def connect_to_dema():
 
 async def get_all_status_pc(club_name: str):
     if club_name == "ufa":
-        response_activeinfo = requests.get("http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/usersessions/activeinfo")
-        response_active = requests.get("http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/usersessions/active")
+        response_activeinfo = requests.get(
+            "http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/usersessions/activeinfo")
+        response_active = requests.get(
+            "http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/usersessions/active")
         status_pc_array = await parsing_text.parsing_user_sessions_active_info(response_activeinfo.text,
                                                                                response_active.text)
         return status_pc_array
--- gizmo/parsing_text.py
+++ gizmo/parsing_text.py
@@ -1,10 +1,55 @@
 import datetime
 import json
-
+import requests
 """          parsing_user_sessions_active_info      ПОКА НЕ РАБОТАЕТ   """
 
 
-async def parsing_user_sessions_active_info(text_for_number_pc: str, text_for_time: str):
+async def parsing_user_sessions_active_info(login : str, password : str, ip_ufa : str, port :str):
+    response = requests.get(
+        "http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/hostcomputers")
+    # test
+    new_text = response.text[response.text.find('[') + 2:len(response.text)]
+    new_text = new_text.replace('[', '')
+    new_text = new_text.replace('{', '')
+    new_text = new_text.replace('}', '')
+    new_text = new_text.replace(']', '')
+    new_text = new_text.replace('"', '')
+    new_array = new_text.split(',')
+    i = 0
+    for g in range(len(new_array)):
+        new_array[g] = new_array[g].split(':')
+    while i < len(response.text):
+        new_i = response.text.find('name', i, len(response.text))
+        if new_i == -1:
+            break
+        new_i = response.text.find(':', new_i, len(response.text)) + 2
+        split_text = response.text.find(',', new_i, len(response.text)) - 1
+        current_pc = response.text[new_i:split_text]
+        i = new_i
+    i = 0
+    new_array_2 = []
+    new_array.pop(len(new_array) - 1)
+    new_array.pop(len(new_array) - 2)
+    new_array.pop(len(new_array) - 3)
+    new_array.pop(len(new_array) - 4)
+    while 13 * (i + 1) <= len(new_array):
+        new_array_2.append([new_array[g + 13 * i] for g in range(13)])
+        i += 1
+    response_test = requests.get("http://google.com")
+    for g in new_array_2:
+        if g[7][1] == 'false':
+            response_test = requests.get("http://" + login + ":" + password + "@" + ip_ufa + ":" + port + "/api/hostcomputers/"+g[1][2])
+            break
+    print(response_test.text)
+    """
+    for g in new_array_2:
+        if g[7][1] == 'false':
+            print(g, "\n")
+    print("\n\n\n")
+    """
+    status_pc_str = 0
+    # test
+    """
     counts_pc = 0
     with open("global_info/info_ufa.txt", "r") as f:
         counts_pc = int(f.readline())
@@ -33,26 +78,9 @@ async def parsing_user_sessions_active_info(text_for_number_pc: str, text_for_ti
             # плойка
             states_pc[len(states_pc) - 1][0] = current_pc
         i = new_i
-    k = 0
-    for j in range(len(states_pc)):
-        if states_pc[j][0] == 0:
-            continue
-        new_i = text_for_time.find('endTime', k, len(text_for_number_pc))
-        if new_i == -1:
-            break
-        new_i = text_for_time.find('"', new_i, len(text_for_number_pc)) + 2
-        split_text = text_for_time.find(',', new_i, len(text_for_number_pc))
-        new_text = text_for_time[new_i:split_text]
-        end_time_year = int(new_text[0:new_text.find("-")])
-        end_time_month = int(new_text[new_text.find("-") + 1: new_text.find("-", new_text.find("-") + 1)])
-        end_time_day = int(new_text[new_text.find("-", new_text.find("-") + 1) + 1:new_text.find("T")])
-        end_time_hours = int(new_text[new_text.find("T") + 1:new_text.find(":")])
-        end_time_minutes = int(new_text[new_text.find(":") + 1:new_text.find(":", new_text.find(":") + 1)])
-        # end_time_seconds = new_text[new_text.find(":", new_text.find(":")+1):new_text.find(".")]
+    i = 0
+    status_pc_str = ""  # удалить после проверок
 
-        current_span = float(new_text)
-        states_pc[j][1] = current_span
-        k = new_i
     status_pc_str = "Текущий статус компьютеров и ps:\n\n⚫ - недоступен\n🔴 - занят\n🟢 - свободен\n\n"
     for i in range(25):  # пока не работает ⚫
         if i < 10:
@@ -115,7 +143,7 @@ async def parsing_user_sessions_active_info(text_for_number_pc: str, text_for_ti
             elif states_pc[i][1] == 0:
                 status_pc_str += "🟢   PS4\n\n"
     status_pc_str += "\nТекущий статус компьютеров и ps:\n⚫ - недоступен\n🔴 - занят\n🟢 - свободен"
-
+    """
     return status_pc_str
 
 
